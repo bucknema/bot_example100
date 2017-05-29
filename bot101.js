6ca7d07b3ce531c7a3696118f832ff1bf293eac7
Mark <bucknema@oregonstate.edu>
2017-05-29 14:08:39 -0400

    variable reset bug fix
    screen name variable was adding characters on every call. added lines to reset the variables

--- bot101.js
+++ bot101.js
@@ -7,8 +7,6 @@
  see https://github.com/BoyCook/TwitterJSClient/blob/master/README.md
  */
 
-var request = require('request');
-
 /*CALLBACK FUNCTIONS*/
 var error = function(err, response, body){
     console.log('ERROR [%s]', err);
@@ -18,39 +16,53 @@ var success = function(data){
     console.log('Data [%s]', data);
 };
 
+/*Variables to hold parsed tweet info*/
 var tweet_content = "";
 var tweet_screenName = "";
+var tweet_string ="";
+var tweet_id = 0; //internal ID
+var tweet_id_str =""; //external ID included in Tweet's unique URL
 
-
+/*Call back function for when the response from getSearch comes back*/
 var successfulSearch = function(data){
 
     //console.log(data);
+
+    /*reset variables*/
+    tweet_content= "";
+    tweet_screenName= "";
+    tweet_string ="";
+    tweet_id = 0;
+    tweet_id_str = "";
 
     /*data comes back in JSON format. You can parse this with JSON.parse()!*/
     var response = JSON.parse(data);
 
     /*Now we can access specific properties of the response*/
     /*the statuses property has an array with most of what we are interested in*/
+    /*Uncomment this and examine response.statuses in your console to see the info you have
+    access to!
+     */
     //console.log(response.statuses);
     //console.log("\n\n");
 
     /*Log the contents of the tweet we found with hashtag #bot to the console*/
   //  console.log("I searched for a tweet containing \"#bot\" and I found this tweet: \n");
   //  console.log("\""+response.statuses[0].text+"\"");
-    tweet_content += response.statuses[0].text;
+    tweet_content = response.statuses[0].text;
   //  console.log("\n\n");
 
     /*Log the user name of who tweeted it to the console*/
    // console.log("user name of person who tweeted it: \n");
    // console.log("@"+ response.statuses[0].user.screen_name);
-    tweet_screenName += response.statuses[0].user.screen_name;
+    tweet_screenName = response.statuses[0].user.screen_name;
    // console.log(tweet_screenName);
    // console.log("\n\n\n");
 
     /*Log the user name of who tweeted it to the console*/
    // console.log("Internal tweet id for the tweet I found: \n");
    // console.log(response.statuses[0].id);
-    var tweet_id = response.statuses[0].id;
+    tweet_id = response.statuses[0].id;
    // console.log("\n\n");
 
     var tweet_id_str = response.statuses[0].id_str;
@@ -60,10 +72,10 @@ var successfulSearch = function(data){
    // console.log("\n\n");
 
     /*respond to account that was tweeting about bots!*/
-    var tweet_string = "Hi @";
+    tweet_string = "Hi there @";
     tweet_string += tweet_screenName;
     //console.log(tweet_string);
-    tweet_string += ", you used the hashtag #bots. I am a Node.js bot! Greetings :)";
+    tweet_string += ", you used the hashtag #bots. I am a Node.js bot! Greetings, friend :)";
 
     twitter.postTweet({status: tweet_string}, error, success);
 
@@ -74,12 +86,12 @@ var Twitter = require('twitter-node-client').Twitter;
 /*Credentials retrieved from your twitter apps dashboard go here:*/
 //replace YOUR_KEY as appropriate
 var config = {
-    "consumerKey": "YOUR_KEY",
-    "consumerSecret": "YOUR_KEY",
-    "accessToken": "YOUR_KEY",
-    "accessTokenSecret": "YOUR_KEY",
+    "consumerKey": "xxxxx",
+    "consumerSecret": "xxxxx",
+    "accessToken": "xxxxx",
+    "accessTokenSecret": "xxxxx",
     "callBackUrl": "XXX"
-}
+};
 
 /*Set-up*/
 var twitter = new Twitter(config);

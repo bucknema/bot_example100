db7cb50b67066ad7ee08680ddf7622a5ea29cecf
Mark <bucknema@oregonstate.edu>
2017-05-29 01:22:05 -0400

    Twitter bot example
    A twitter bot with node.js that searches for #bot and says hi to the user who tweeted with that hashtag.

---
+++ bot101.js
@@ -0,0 +1,94 @@
+/**
+ * Created by Mark Buckner on 5/28/2017.
+ */
+
+/*
+ * for twitter-node-client help:
+ see https://github.com/BoyCook/TwitterJSClient/blob/master/README.md
+ */
+
+var request = require('request');
+
+/*CALLBACK FUNCTIONS*/
+var error = function(err, response, body){
+    console.log('ERROR [%s]', err);
+};
+
+var success = function(data){
+    console.log('Data [%s]', data);
+};
+
+var tweet_content = "";
+var tweet_screenName = "";
+
+
+var successfulSearch = function(data){
+
+    //console.log(data);
+
+    /*data comes back in JSON format. You can parse this with JSON.parse()!*/
+    var response = JSON.parse(data);
+
+    /*Now we can access specific properties of the response*/
+    /*the statuses property has an array with most of what we are interested in*/
+    //console.log(response.statuses);
+    //console.log("\n\n");
+
+    /*Log the contents of the tweet we found with hashtag #bot to the console*/
+  //  console.log("I searched for a tweet containing \"#bot\" and I found this tweet: \n");
+  //  console.log("\""+response.statuses[0].text+"\"");
+    tweet_content += response.statuses[0].text;
+  //  console.log("\n\n");
+
+    /*Log the user name of who tweeted it to the console*/
+   // console.log("user name of person who tweeted it: \n");
+   // console.log("@"+ response.statuses[0].user.screen_name);
+    tweet_screenName += response.statuses[0].user.screen_name;
+   // console.log(tweet_screenName);
+   // console.log("\n\n\n");
+
+    /*Log the user name of who tweeted it to the console*/
+   // console.log("Internal tweet id for the tweet I found: \n");
+   // console.log(response.statuses[0].id);
+    var tweet_id = response.statuses[0].id;
+   // console.log("\n\n");
+
+    var tweet_id_str = response.statuses[0].id_str;
+    RT_id = response.statuses[0].id_str;
+    //console.log("URL of the tweet I found ");
+   // console.log("http://twitter.com/"+tweet_screenName+"/status/"+tweet_id_str);
+   // console.log("\n\n");
+
+    /*respond to account that was tweeting about bots!*/
+    var tweet_string = "Hi @";
+    tweet_string += tweet_screenName;
+    //console.log(tweet_string);
+    tweet_string += ", you used the hashtag #bots. I am a Node.js bot! Greetings :)";
+
+    twitter.postTweet({status: tweet_string}, error, success);
+
+};
+
+var Twitter = require('twitter-node-client').Twitter;
+
+/*Credentials retrieved from your twitter apps dashboard go here:*/
+//replace YOUR_KEY as appropriate
+var config = {
+    "consumerKey": "YOUR_KEY",
+    "consumerSecret": "YOUR_KEY",
+    "accessToken": "YOUR_KEY",
+    "accessTokenSecret": "YOUR_KEY",
+    "callBackUrl": "XXX"
+}
+
+/*Set-up*/
+var twitter = new Twitter(config);
+
+/*searches for #bot tweets and replies to them!*/
+var botHashtag_searchAndReply = function() {
+    /*Code to retweet first tweet found (i.e., most recent) with hashtag #bot*/
+    twitter.getSearch({'q':'#bot','count': 1}, error, successfulSearch);
+}
+
+/*Use JavaScript's timer function setInterval() to automatically schedule an action*/
+setInterval(botHashtag_searchAndReply,900000);
